c746b4a0f957a46f82aec65844a2211336565e0b
1f10220081 <s1f102200817@iniad.org>
2023-07-06 12:43:08 +0900

    修正14

--- todo/views.py
+++ todo/views.py
@@ -6,10 +6,12 @@ from todo.models import Task
 # Create your views here.
 
 def index(request):
+
     if request.method == 'POST':
         task = Task(
-            title = request.POST['title'], 
-            due_at = make_aware(parse_datetime(request.POST['due_at'])))
+            title=request.POST['title'],
+            due_at=make_aware(parse_datetime(request.POST['due_at']))
+        )
         task.save()
 
     if request.GET.get('order') == 'due':
@@ -20,4 +22,5 @@ def index(request):
     context = {
         'tasks': tasks
     }
+
     return render(request, 'todo/index.html', context) 
